eff8a0666a3dcd479585ec50c8f33a7ebe505dcc
Antek <un.tek2008@gmail.com>
2025-03-29 11:14:56 +0100

    updated main && generated index

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import kaplay, { GameObj } from "kaplay";
 import { Card } from "./card";
-import "kaplay/global"; 
+import "kaplay/global"; // uncomment if you want to use without the k. prefix
+import { Deck } from "./deck";
+import {Game} from "./game";
 
 kaplay();
 
@@ -8,6 +10,18 @@ loadRoot("./");
 loadSprite("cards", "sprites/cards.png", {
   sliceX: 9,
   sliceY: 6,
+});
+
+scene("game", () => {
+  setBackground(rgb(0, 150, 50));
+  
+  const game: Game = new Game();
+  game.start();
+  
+  add([
+    text(game.players[0].name),
+    {}
+  ])
 });
 
 const card: Card = new Card("clubs", "2");
